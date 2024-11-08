a88823bd079f32acdd6359482506a767baba0c54
Yash <kudveyash@gmail.com>
2024-11-08 16:44:07 +0530

    sidebar collapse function and change in font

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import assets from '../../assets/assets.js'
 
 const Sidebar = () => {
+
+    const[extended,setExtended] = useState(false)
   return (
     <div className='sidebar'>
       <div className="top">
+
+        {/* <div className="menu-icon">
         <img src={assets.menu_icon} alt="menu-icon" />
+        </div> */}
+        <img onClick={()=>setExtended(prev=>!prev)} src={assets.menu_icon} className='menu' alt="menu-icon" />
 
         <div className="new-chat">
             <img src={assets.plus_icon} alt="plus_icon" />
-            <p>New Chat</p>
+            {extended?<p>New Chat</p>:null}
         </div>
 
+        {extended? 
         <div className="recent">
-            <p className="recent-title">
-                Recent
-            </p>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="message_icon" />
-                <p>What is react...</p>
-            </div>
+        <p className="recent-title">
+            Recent
+        </p>
+        <div className="recent-entry">
+            <img src={assets.message_icon} alt="message_icon" />
+            <p>What is react...</p>
         </div>
+    </div>
+        :null}
       </div>
 
     {/*  */}
@@ -29,17 +37,17 @@ const Sidebar = () => {
       <div className="bottom">
         <div className="bottom-item recent-entry">
             <img src={assets.question_icon} alt="" />
-            <p>Help</p>
+            {extended?<p>Help</p>:null}
         </div>
 
         <div className="bottom-item recent-entry">
             <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
+            {extended?<p>Activity</p>:null}
         </div>
 
         <div className="bottom-item recent-entry">
             <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
+            {extended?<p>Setting</p>:null}
         </div>
       </div>
     </div>
