0bafd81aa13445a4f914821c88f6705fa3e5e3c6
Yash <kudveyash@gmail.com>
2024-11-13 17:48:48 +0530

    bottom section added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -37,6 +37,22 @@ const Main = () => {
                     <img src={assets.compass_icon} alt="Compass_icon" />
                 </div>
             </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here' />
+
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+
+                <p className="bottom-info">
+                    Gemini may display inaccurate info, including about people, so double-check its response. Your privacy and Gemini Apps.
+                </p>
+            </div>
         </div>
               
     </div>
